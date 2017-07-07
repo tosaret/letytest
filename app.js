102ced50ef8db1578034ad603f1d967c0733d705
Denys Zabohonskyi <2saret@gmail.com>
2017-07-07 17:20:43 +0300

    test

---
+++ app.js
@@ -0,0 +1,62 @@
+var login = $('#email2');
+var password = $('#password2');
+
+$(document).ready(function() {
+
+	sectionShow(window.location.hash);
+
+	$('form').validator();
+
+	$.getJSON( "http://www.json-generator.com/api/json/get/bZTcBnsIgO", function( json ) {
+
+		$.each(json, function( index ) {
+	  		$('table tbody').append('<tr><td>' + json[index].title +
+	  	 	'</td><td><img src="' + json[index].cover + '" /></td><td>' +
+	  	 	json[index].description + '</td><td>' + json[index].link + '</td></tr>');
+		});
+		$('table').DataTable({
+			searching: false,
+			info: false,
+			lengthChange: false
+		});
+
+	});
+
+});
+
+$(window).on('hashchange', function() {
+	sectionShow(window.location.hash);
+});
+
+function sectionShow(id) {
+	$('.section').addClass('hidden');
+	$(id).toggleClass('hidden');
+	if ( id == '#shops' && localStorage.getItem('loggedIn') != true || id == '' ) {
+		showLogin();
+	}
+}
+
+function showLogin() {
+	sectionShow('#login');
+}
+
+function userRegister() {
+	localStorage.setItem('login', login.val());
+	localStorage.setItem('password', password.val());
+	showLogin();
+}
+
+function userLogin() {
+	var regLogin = localStorage.getItem('login');
+	var regPassword = localStorage.getItem('password');
+	var enteredLogin = $('#email');
+	var enteredPassword = $('#password');
+
+	if ( enteredLogin.val() == regLogin && enteredPassword.val() == regPassword ) {
+		var loggedIn = localStorage.setItem('loggedIn', true);
+		sectionShow('#shops');
+		window.location.hash = '#shops';
+	} else {
+		alert('Wrong username or password! Please try again.');
+	}
+}
